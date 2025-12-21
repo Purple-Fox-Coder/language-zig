55b63ad58b55b028450dd06491328ad51cff9598
Anna Robinson <annatheprogrammer777@gmail.com>
2025-12-21 15:32:25 -0600

    feat: Import the baseline settings from the grammar repo

---
+++ grammars/tree-sitter/indents.scm
@@ -0,0 +1,30 @@
+; https://github.com/tree-sitter-grammars/tree-sitter-zig/blob/master/queries/indents.scm
+
+[
+  (block)
+  (struct_declaration)
+  (enum_declaration)
+  (union_declaration)
+  (switch_expression)
+  (if_expression)
+  (while_expression)
+  (for_expression)
+  (initializer_list)
+] @indent.begin
+
+(block
+  "}" @indent.end)
+
+[
+  "("
+  ")"
+  "["
+  "]"
+  "{"
+  "}"
+] @indent.branch
+
+[
+  (comment)
+  (multiline_string)
+] @indent.ignore
